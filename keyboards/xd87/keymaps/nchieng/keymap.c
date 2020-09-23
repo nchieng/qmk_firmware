94c54611f2618e0f02bfd78ecb0c4d809277aa3d
Nick Chieng <nickch85@gmail.com>
2020-09-23 23:45:37 +1000

    Implement tap dance parenthesis

--- keyboards/xd87/keymaps/nchieng/keymap.c
+++ keyboards/xd87/keymaps/nchieng/keymap.c
@@ -16,13 +16,13 @@
 #include QMK_KEYBOARD_H
 
 enum {
-  TD_LSHIFT_LBRC,
-  TD_RSHIFT_RBRC
+  TD_LSHIFT_LPRN,
+  TD_RSHIFT_RPRN
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_LSHIFT_LBRC] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_LBRC),
-  [TD_RSHIFT_RBRC] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_RBRC)
+  [TD_LSHIFT_LPRN] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_LPRN),
+  [TD_RSHIFT_RPRN] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_RPRN)
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -32,7 +32,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC,            KC_INS,  KC_HOME, KC_PGUP,
       KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_DEL,  KC_END,  KC_PGDN,
       KC_CAPS,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,
-      TD(TD_LSHIFT_BRC),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           TD(TD_RSHIFT_RBRC),                     KC_UP,
+      TD(TD_LSHIFT_LPRN),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          TD(TD_RSHIFT_RPRN),          KC_UP,
+      // KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                     KC_RSFT,                     KC_UP,
       KC_LCTL,  KC_LGUI,KC_LALT,                       KC_SPC,                                  KC_RALT, MO(1),    KC_APP,   KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
--- keyboards/xd87/keymaps/nchieng/readme.md
+++ keyboards/xd87/keymaps/nchieng/readme.md
@@ -1,1 +1,10 @@
 # The default keymap for xd87
+
+
+```
+$ qmk compile -kb xd87 -km nchieng
+# Connect keyboard in bootloader mode
+$ sudo dfu-programmer atmega32u4 erase
+$ sudo dfu-programmer atmega32u4 flash xd87_nchieng.hex
+$ sudo dfu-programmer atmega32u4 reset
+```
